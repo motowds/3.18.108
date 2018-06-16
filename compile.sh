ada36bf8f2f1bd76458946725010e654830d9775
iykequame <izaqkull@yahoo.com>
2018-06-16 23:56:50 +0000

    OincBugxanDroid

--- compile.sh
+++ compile.sh
@@ -23,8 +23,9 @@ mkdir -p outdir
 export ARCH=arm
 export ARCH_MTK_PLATFORM=mt6735
 #export CROSS_COMPILE=~/arm-eabi-4.9/bin/arm-eabi-
-export CROSS_COMPILE=$PWD/arm-gnu-7.x/bin/arm-gnu-linux-androideabi-
+#export CROSS_COMPILE=$PWD/arm-gnu-7.x/bin/arm-gnu-linux-androideabi-
 #export CROSS_COMPILE=$PWD/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE=/home/izaqkull/MotoE4/linaro_631_arm-eabi/bin/arm-eabi-
 export KBUILD_BUILD_USER="izaqkull"
 export KBUILD_BUILD_HOST="OSX-Strec"
 
@@ -43,15 +44,15 @@ echo "$purple▐▌▐█▌▐█▄▄▌██▐█▌██▐█▌▐█�
 echo
 echo
 echo "$blue***********************************************"
-echo "          Compiling Jennie™ Kernel...          "
+echo "          Compiling Oinc.Bugx™.anDroid Kernel...          "
 echo "***********************************************$nocol"
 echo ""
 #woods defconfig
 make -C $PWD O=outdir ARCH=arm woods_defconfig
 #
 make -j4 -C $PWD O=outdir ARCH=arm
-echo "$yellow Copying to outdir/Jennie $nocol"
-cp outdir/arch/arm/boot/zImage-dtb outdir/Jennie/Image
+echo "$yellow Copying to outdir/OincBugxanDroid $nocol"
+cp outdir/arch/arm/boot/zImage-dtb outdir/OincBugxanDroid/Image
 
 if ! [ -f $ZIMAGE ];
 then
@@ -72,21 +73,21 @@ echo "$cyan█ ▄▄  ██   ▄█▄    █  █▀ ▄█    ▄     ▄�
   ▀     █          ▀       █   ██       
        ▀  "
 echo  "$cyan***********************************************"
-echo "          Packing Jennie™ Kernel...          "
+echo "          Packing Oinc.Bugx™.anDroid Kernel...          "
 echo  "***********************************************$nocol"
 echo ""
-echo  "$yellow Putting Jennie™ Kernel in Recovery Flashable Zip $nocol"
+echo  "$yellow Putting Oinc.Bugx™.anDroid Kernel in Recovery Flashable Zip $nocol"
 #using lazy kernel flasher
 cd outdir
-cd Jennie
+cd OincBugxanDroid
     if 
-    [ -f outdir/Jennie/out_done ] 
+    [ -f outdir/OincBugxanDroid/out_done ] 
     then
     rm -rf out_done
     else
     make
     mkdir -p out_done
-    cp Jennie.N7x_woods*zip* out_done
+    cp OincBugxanDroid-Ngt-woods*zip* out_done
     cd ../../
     sleep 0.6;
     echo ""
@@ -94,8 +95,8 @@ cd Jennie
     echo "" "Done Making Recovery Flashable Zip"
     echo ""
     echo ""
-    echo "" "Locate Jennie™ Kernel in the following path : "
-    echo "" "outdir/Jennie/out_done"
+    echo "" "Locate Oinc.Bugx™.anDroid Kernel in the following path : "
+    echo "" "outdir/OincBugxanDroid/out_done"
     echo ""
 echo  "$green▄   █ ▄▄  █    ████▄ ██   ██▄   ▄█    ▄     ▄▀  
    █  █   █ █    █   █ █ █  █  █  ██     █  ▄▀    
@@ -106,7 +107,7 @@ echo  "$green▄   █ ▄▄  █    ████▄ ██   ██▄   ▄�
                         ▀ "                         
     echo
     echo  "$blue***********************************************"
-    echo "      Uploading Jennie™ Kernel to Web[https://transfer.sh/]"
+    echo "      Uploading Oinc.Bugx™.anDroid Kernel to Web[https://transfer.sh/]"
     echo  "***********************************************$nocol"
     echo ""
     echo " l.o.a.d.i.n.g..."
@@ -114,7 +115,7 @@ echo  "$green▄   █ ▄▄  █    ████▄ ██   ██▄   ▄�
     echo "   please wait..."
     sleep 0.1;
     echo ""
-    curl --upload-file outdir/Jennie/out_done/Jennie.N7x_woods*.zip https://transfer.sh/Jennie.N7x_woods_Nougat_$BUILD_START.zip
+    curl --upload-file outdir/OincBugxanDroid/out_done/OincBugxanDroid-Ngt-woods*.zip https://transfer.sh/OincBugxanDroid-Ngt-woods_Nougat_$BUILD_START.zip
     echo ""
     echo ""
     echo " uPLOADING dONE !!!"
